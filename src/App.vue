7b5fc269b5d480f3f7004656d0ef2bc42a68b5db
DotSea <85677992+DavidFu95@users.noreply.github.com>
2021-11-24 14:04:47 +0800

    feat: add function of getting access token

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,31 @@
 </template>
 
 <style lang="scss">
-@import 'reset-css';
 @import './assets/base.scss';
 </style>
 
 <script>
+import JsSHA from 'jssha';
 import TheHeader from './components/TheHeader.vue';
 
-export default { name: 'App', components: { TheHeader } };
+export default {
+  name: 'App',
+  components: { TheHeader },
+
+  provide() {
+    return {
+      getAuthorizationHeader() {
+        const AppID = process.env.VUE_APP_API_ID;
+        const AppKey = process.env.VUE_APP_API_KEY;
+        const GMTString = new Date().toGMTString();
+        const ShaObj = new JsSHA('SHA-1', 'TEXT');
+        ShaObj.setHMACKey(AppKey, 'TEXT');
+        ShaObj.update(`x-date: ${GMTString}`);
+        const HMAC = ShaObj.getHMAC('B64');
+        const Authorization = `hmac username="${AppID}", algorithm="hmac-sha1", headers="x-date", signature="${HMAC}"`;
+        return { Authorization, 'X-Date': GMTString };
+      },
+    };
+  },
+};
 </script>
